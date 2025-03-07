64965156d9de4f6ed9ab578e16995be76e13f67d
Feranen <127853064+Feranen@users.noreply.github.com>
2025-03-07 16:19:20 +0100

    Useless comments

--- php/rezerwacja.php
+++ php/rezerwacja.php
@@ -1,14 +1,10 @@
 <?php
-// Connection details
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "egzaminac";
 
-// Create a connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check the connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
@@ -16,17 +12,16 @@ if ($conn->connect_error) {
 $message = ""; // Variable to store the message
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get form data
     $Date = $_POST["Data"];
     $Osoby = trim($_POST["Osoby"]);
     $Phone = trim($_POST["Phone"]);
 
     if (!empty($Date) && !empty($Osoby) && !empty($Phone)) {
-        // Prepare and bind
+
         $stmt = $conn->prepare("INSERT INTO rezerwacje (data_rez, liczba_osob, telefon) VALUES (?, ?, ?)");
         $stmt->bind_param("sis", $Date, $Osoby, $Phone);
 
-        // Execute the query
+
         if ($stmt->execute()) {
             $message = "Rezerwacja udana!";
         } else {
